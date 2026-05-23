da134ca5156cd69498c1106724d4e70c0db790e6
Claire Jepngetich <clairejepngetich@gmail.com>
2026-05-24 04:27:56 +1100

    data_types.py

---
+++ python-foundations-assignment/data_types.py
@@ -0,0 +1,42 @@
+// Personal Bio Generator
+name = "Claire"
+age = 20
+height = 5.6
+favorite_tech_field = "Data Science"
+is_student = True
+
+print(f"My name is {name}. I am {age} years old, {height} feet tall, and I love {favorite_tech_field}. Student Status: {is_student}")
+
+// Type Checker
+integer_num = 10
+float_num = 3.14
+text = "Python"
+status = True
+
+print(type(integer_num))
+print(type(float_num))
+print(type(text))
+print(type(status))
+
+// Data Conversion
+num = 100
+print(str(num))
+
+decimal = 12.9
+print(int(decimal))
+
+string_number = "50"
+print(int(string_number))
+
+// User Information
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+country = input("Enter your country: ")
+
+print(f"Hello {user_name}, you are {user_age} years old and you come from {country}.")
+
+// Temperature Converter
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius}°C is equal to {fahrenheit}°F")
